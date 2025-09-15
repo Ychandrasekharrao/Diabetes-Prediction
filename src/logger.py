6ed59f03a50b41f739b41f2af4bf993dd09b3a19
Chandra Sekhar <sekher157@gmail.com>
2025-09-15 18:23:39 +0530

    2nd commit

---
+++ src/logger.py
@@ -0,0 +1,16 @@
+import logging
+import os
+from datetime import datetime
+
+log_file = f"{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.log"
+logs_path = os.path.join(os.getcwd(), "logs", log_file)
+os.makedirs(os.path.dirname(logs_path), exist_ok=True)
+
+log_file_path = os.path.join(logs_path, log_file)
+
+logging.basicConfig(
+    filename=log_file_path,
+    format="[%(asctime)s] %(levelname)s - %(message)s",
+    level=logging.INFO,
+)
+
